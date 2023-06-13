b42919d734254dab86d0e2e0cf763b6dad0ce38e
a.pushkin <a.v.pushkin@gmail.com>
2023-06-13 12:42:40 +0300

    fix task_3

--- task.js
+++ task.js
@@ -1,16 +1,34 @@
 const progress = document.getElementById('progress')
-const sendBtn = document.getElementById('send')
+const form = document.getElementById('form')
 
 
-sendBtn.addEventListener('click', () => {
-    const form = document.getElementById('form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
     const formData = new FormData(form)
     const request = new XMLHttpRequest()
     request.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload')
-    request.send(formData)
     request.upload.addEventListener('progress', e => {
         const percent = e.loaded / e.total
         progress.value = percent
     })
-})
 
+    // Вывод ответа сервера
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4)
+        console.log(`Ответ сервера: ${request.responseText}`)
+    })
+
+    // Проверка статускода
+    request.onreadystatechange = () => {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            let code = request.status
+            if (code === 201) {
+                console.log(`Файл успешно загружен. Код ответа - ${code}`);
+            } else {
+                console.error(`Ошибка при загрузке файла. Код ответа - ${code}`);
+            }
+        }
+    };
+
+    request.send(formData)
+})
